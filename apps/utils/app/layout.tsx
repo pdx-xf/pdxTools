680cc5bd30f04d6571c005e4d55af8c620721326
pdx <qiuqfang98@qq.com>
2025-08-19 14:07:54 +0800

    新增@vercel/speed-insights依赖并在布局中集成速度分析功能
    - 在package.json中添加了@vercel/speed-insights作为依赖，版本为1.2.0。
    - 更新pnpm-lock.yaml以反映新依赖的版本信息。
    - 在布局组件中引入SpeedInsights组件，增强应用的性能监测能力。
    
    此更新提升了应用的性能分析功能，为优化用户体验提供支持。

--- apps/utils/app/layout.tsx
+++ apps/utils/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -57,6 +58,7 @@ export default function RootLayout({
             <Header />
             {children}
             <Analytics />
+            <SpeedInsights />
           </div>
         </ThemeProvider>
         <Toaster />
